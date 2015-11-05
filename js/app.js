f40831424a5adc86a5eb942b9499080875a60b60
Javier Salazar <jav_salazar@yahoo.com>
2015-11-04 20:54:20 -0800

    refactor: remove comments and use single quote rather than double

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 /* TODO:
     jumping' sound lags if move player too fast, need to fix
-    levels, get working
-    decrement lives on fail attempt
 
 current features:
     bugs move randomly from L->R or R->L
@@ -10,71 +8,98 @@ current features:
     points based on how fast you reach goal
     display, SCORE and NO of LIVES
     LEVELS - add life on reach level
-
-
-    ENEMIES:
-            y = 66, 150, or 234
-    PLAYER:
-            y= 74, 158, or 242
 */
 
+ // as level index increase, difficulty increase
 var levels = [
-    {
-        "points": 20,
-        "bugs": 2,
-        "speed": 50
-    },
-    {
-        "points": 50,
-        "bugs": 3,
-        "speed": 100
-    },
-    {
-        "points": 80,
-        "bugs": 5,
-        "speed": 200
-    },
-    {
-        "points": 100,
-        "bugs": 6,
-        "speed": 300
-    },
-    {
-        "points": 150,
-        "bugs": 7,
-        "speed": 400
-    },
-    {
-        "points": 200,
-        "bugs": 8,
-        "speed": 500
-    }
-];
+        {
+            points: 50,  // point required to get to this level
+            bugs: 1,
+            speed: 50
+        },
+        { // level 1
+            points: 100,
+            bugs: 3,
+            speed: 100
+        },
+        {
+            points: 200,
+            bugs: 4,
+            speed: 150
+        },
+        {
+            points: 300,
+            bugs: 5,
+            speed: 220
+        },
+        {
+            points: 450,
+            bugs: 6,
+            speed: 300
+        },
+        {
+            points: 600,
+            bugs: 7,
+            speed: 420
+        },
+        {
+            points: 700,
+            bugs: 8,
+            speed: 500
+        },
+        {
+            points: 800,
+            bugs: 9,
+            speed: 600
+        },
+        {
+            points: 10000,
+            bugs: 10,
+            speed: 700
+        }
+    ],
+    goodies = [ // determine 'reward' for each goodie.
+        {
+            url: "images/Heart.png",
+            life: 1,
+            points: 0
+        },
+        {
+            url: "images/gem-blue.png",
+            life: 0,
+            points: 20
+        },
+        {
+            url: "images/gem-orange.png",
+            life: 0,
+            points: 50
+        },
+        {
+            url: "images/gem-green.png",
+            life: 1,
+            points: 75
+        }
+    ];
 
-// Enemies our player must avoid
+/****** ENEMY RELATED *****/
+
 var Enemy = function(yStart) {
     // Variables applied to each of our instances go here
-
     this.speed = this.getSpeed();  // get speed
-
-    console.log("bug "+ yStart + " speed: ", this.speed);
-
     this.fromDirection = this.getStartLocation();  // left or right
     
     if ( yStart > 2 ) {
         yStart = yStart % 3; //only have 3 rows, make sure to add them there
     } 
-    this.y = yStart * 84 + 66;  // which row to place, 66 is offset from top
-    this.x = Resources.getRandom(-90,495);
+    this.x = Resources.getRandom(-90,495); // random place between canvas
+    this.y = yStart * 83 + 66;  // which row to place, 66 is offset from top
 
-    // get correct facing image and place at proper edge
+    // get correct facing image
     if (this.fromDirection === 'left'){
         this.sprite = 'images/enemy-bug.png';
-        // this.x = -90; //one position off to left
     }else{
         this.sprite = 'images/enemy-bug-r.png';
-        // this.x = 495; //one position off to right
-        this.speed *= -1;
+        this.speed *= -1; //moving left
     }
 };
 
@@ -87,13 +112,12 @@ Enemy.prototype.render = function() {
 Enemy.prototype.getStartLocation = function() {
     // get random number 1 or 2
     var start = Resources.getRandom(1,2);
-    return ( start === 1 ) ? "left" : "right";
-}
+    return ( start === 1 ) ? 'left' : 'right';
+};
 
 Enemy.prototype.getSpeed = function () {
     return Math.floor( ( Math.random() * levels[player.level].speed ) + 50 );
-    // return Math.floor( ( Math.random() * 175 ) + 50 )
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -113,49 +137,25 @@ Enemy.prototype.update = function(dt) {
     this.x += ( this.speed * dt );
 };
 
-Enemy.prototype.reset = function (level) {
-    var allEnemies = [],
-        totalEnemies = level.bugs; //3,
-
-    for( var count = 0; count < totalEnemies; count++ ){
-        // count determines the row the bug appears on
-        allEnemies.push( new Enemy(count));
-
-        // play enemy/game sound after last enemy pushed into array
-        if ( allEnemies[totalEnemies - 1]){
-            // allEnemies[count].enemySound();  //JAV-UNCOMMENT FOR SOUND>>>
-        }
-    }
-}
-
-Enemy.prototype.enemySound = function(action) {
-
-    var sound = document.getElementById("bug-sound");
-    sound.pause();
-
-    if ( action === "pause"){
-        return;
-    }
-    sound.src = "../sound/groovy.m4a";
-    sound.volume = 0.3;
-    sound.play();
-}
+/****** PLAYER RELATED *****/
 
 var Player = function( xStart, yStart ){
     // image chosen for the player
-    this.sprite = 'images/char-boy.png';
-
+    // this.sprite = 'images/char-boy.png';
+    
+    this.character = Resources.getRandom(0,4); // index of character (default)
+    this.sprite = Resources.cycleCharacter(this.character, true);
     // player will start at the given location, center bottom row by default.
     this.x = xStart || 202;
-    this.y = yStart || 410;
+    this.y = yStart || 413;
     this.startTime = Date.now();
 
     // define x and y deltas
     this.xDelta = 101;
-    this.yDelta = 84;
+    this.yDelta = 83;
     //define player boundaries
-    this.yUpBound = -10;
-    this.yDownBound = 410;
+    this.yUpBound = -2;
+    this.yDownBound = 413;
     this.xLeftBound = 0;
     this.xRightBound = 404;
 
@@ -163,45 +163,54 @@ var Player = function( xStart, yStart ){
     this.score = 0;
     this.lives = 3;
     this.level = 0;
-    this.inState = "play";
-    // return this;
+    this.inState = 'play';
+    this.music = '';
 };
 
 Player.prototype.render = function() {
     
-    if (this.inState == "over"){
+    if (this.inState == 'over'){
         // canvas is 505 x 606
         this.drawText(ctx, "Game Over!", 505 * 0.5, 606 * 0.5 );  
         this.drawText(ctx, "(press 'r' to play again)", 505 * 0.5, 606 * 0.75 );  
     }
 
-    if (this.inState == "levelUp"){
-        // canvas is 505 x 606
-
+    if (this.inState == 'levelUp'){
+        // canvas is 505 x 606 -- center text
         this.drawText(ctx, "New level: " + this.level, 505 * 0.5, 606 * 0.5 );  
     }
 
     this.drawScore();
     this.drawLives();
+    this.drawHelp();
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    
 };
 
 Player.prototype.playSound = function (tune) {
-    var sound = document.getElementById("player-sound");
+    var sound = document.getElementById('player-sound');
     
     sound.pause();
-    sound.src = tune || "../sound/jump.m4a";  
+    sound.src = tune || '../sound/jump.m4a';  
     sound.volume = 0.7;
     sound.play();
-}
+};
 
 Player.prototype.drawScore = function () {
-    var sc = "Score: " + this.score;
-    ctx.font = "22pt Impact";
-    ctx.fillStyle = "white";
+    var sc = 'Score: ' + this.score;
+    ctx.font = '22pt Impact';
+    ctx.fillStyle = 'white';
     ctx.fillText(sc, 20, 90);
-}
+};
+
+Player.prototype.drawHelp = function () {
+    var sc = '\'h\' for help';
+    ctx.save();
+    ctx.font = '10pt Impact ';
+    ctx.fillStyle = 'black';
+    ctx.fillText(sc, 20, 570);
+    ctx.restore();
+};
 
 Player.prototype.drawLives = function () {
     ctx.save();
@@ -213,31 +222,53 @@ Player.prototype.drawLives = function () {
     }
 
     ctx.restore();
-}
+};
 
 Player.prototype.drawText = function (ctx, text, x, y) {
     ctx.save();
-    ctx.font = "36pt Impact";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "white";
+    ctx.font = '36pt Impact';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
     ctx.fillText(text, x, y);
     //outline
-    ctx.strokeStyle = "black";
+    ctx.strokeStyle = 'black';
     ctx.lineWidth = 2;
     ctx.strokeText(text, x, y);
     ctx.restore();
-}
+};
 
 Player.prototype.handleInput = function (key) {
     
-    if ( this.inState === "over"  && key === "r"){
+    if ( key === 'c'){
+        var char = this.character !== 4 ? this.character + 1 : 0;
+        Resources.cycleCharacter(char);
+    }
 
-        console.log("I want to play AGAING!!!");
+    if ( key === 'h'){
+        this.inState = this.inState === 'play' ? 'pause' : 'play';
+        document.querySelector('.flip-container').classList.toggle('hover');
+    }
+
+    if ( key === 'm'){
+        
+        var state = this.music;
+
+        this.music = ( state ) ?  false :  true;
+
+        Resources.bgSound(this.music);
+    }
+
+    
+    // start new game if game is over and click on 'r'
+    if ( (this.inState === 'over'  || this.inState === 'pause')  && key === 'r'){
+
+        console.log('I want to play AGAING!!!');
         player = new Player();
         allEnemies = Resources.createEnemies();
     }
 
-    if ( this.inState !== "crash" && this.inState !== "over" ){
+    // move player if didn't hit enemy and game is not over or paused
+    if ( this.inState !== 'crash' && this.inState !== 'over' && this.inState !== 'pause' ){
         switch( key )
         {
             // not past leftBound AND not on top row
@@ -280,97 +311,114 @@ Player.prototype.handleInput = function (key) {
                 break;
         }
     }
-}
+};
 
 Player.prototype.levelReached = function () {
 
     this.totalTime = (Date.now() - this.startTime) / 1000.0;
-    console.log("Time to reach goal: %s", this.totalTime);
+    console.log('Time to reach goal: %s', this.totalTime);
     this.playSound('../sound/levelup.m4a');
-    this.score += Math.round(50 / this.totalTime);
-    console.log(" Score is : ", Math.round(50 / this.totalTime));
+    this.score += Math.round(50 / this.totalTime);  // use 50 as a random number of points to give
+    console.log(' Score is : ', Math.round(50 / this.totalTime));
 
-    if ( this.score > levels[this.level].points){
-        console.log("NEW LEVEL REACHED");
-        this.level += 1;  //move to next leveDzl
+    // check to see if enough points to move to next level BUT not beyond level 10 (last one)
+    if ( this.score > levels[this.level].points  && this.level !== 10){
+        console.log('NEW LEVEL REACHED');
+        this.level += 1;  //move to next level
         this.lives += 1;  //add to lives total
-        this.inState = "levelUp"; // gets reset on restart()
-        console.log("new level is: %s", this.level);
+        this.inState = 'levelUp'; // to show message, gets reset on restart()
+        console.log('new level is: %s', this.level);
         Resources.createEnemies(levels[this.level]);
-        this.restart("levelUp");  
-        return; 
     }
 
     this.restart();
-    
-}
+};
 
 Player.prototype.checkCollision = function  () {
     var currentEnemy,
         enemyLeftEdge,
         enemyRightEdge,
-        playerLeftEdge,
-        playerRightEdge;
+        playerLeftEdge = this.x + 17, //17 is offset from x location, image 67 wide (101-67)/2
+        playerRightEdge = playerLeftEdge + 67; // add width for right edge
+
 
     // check for enemies
     for(var i = 0,numEnemies = allEnemies.length; i < numEnemies; i++) {
         currentEnemy = allEnemies[i];
         enemyLeftEdge = currentEnemy.x,
-        enemyRightEdge = currentEnemy.x + 98, //location plus enemy width
-        playerLeftEdge = this.x + 17, // 17 is offset from x location, image 67 wide (101-67)/2
-        playerRightEdge = this.x + 17 + 67; // add width for right edge
+        enemyRightEdge = enemyLeftEdge + 98, //location plus enemy width
+        enemyTopEdge = currentEnemy.y,
+        enemyBottomEdge = enemyTopEdge + 50;
 
         if ( enemyRightEdge > playerLeftEdge && enemyLeftEdge < playerRightEdge && this.y < currentEnemy.y + 50 && this.y + 50 > currentEnemy.y) {
-            console.log("crash!");
+            console.log('crash!');
             
             this.playSound('../sound/die.m4a');
             this.lives -= 1;
-            this.inState = "crash";
+            this.inState = 'crash';
             this.restart();    
         }
     } 
 
-    // check for hearts
-    heartLeftEdge = heart.x;
-    heartRightEdge = heart.x + 89;
+    // check for goodies
+    var goodieLeftEdge = goodie.x,
+        goodieRightEdge = goodieLeftEdge + 89;
+        goodieTopEdge = goodie.y,
+        goodieBottomEdge = goodieTopEdge + 50;
 
-    // chcek for rocks
+    if ( this.checkEdges(goodieLeftEdge, goodieRightEdge, goodieTopEdge, goodieBottomEdge) ) {
+        
+        console.log('goodie me!');
+        goodie.x = -200;
+        goodie.y= -200;
+        this.playSound('../sound/goodie.m4a');
+        this.lives += goodie.life;
+        this.score += goodie.points;
+    }
+};
 
-    // check for gems  
-}
+Player.prototype.checkEdges = function (oLeft, oRight, oTop, oBottom) {
+    playerLeftEdge = this.x + 14.5, //17 is offset from x location, image ~72 (avg. width for all characters) wide (101-72)/2
+    playerRightEdge = playerLeftEdge + 72, // add width for right edge ~72
+    playerTopEdge = this.y,
+    playerBotomEdge = playerTopEdge + 50;
+
+    return oRight > playerLeftEdge && oLeft < playerRightEdge && playerTopEdge < oBottom && playerBotomEdge > oTop; 
+};
 
 Player.prototype.update = function () {
     this.checkCollision();
-}
+};
 
-Player.prototype.restart = function (levelUp) {
+Player.prototype.restart = function () {
 
     // need to pass reference to 'this' into function
     var that = this;
 
     // wait for sound to stop playing before starting again...
     // and only listen for it once, when we 'crash'
-    onetime(document.getElementById("player-sound"), "ended", function() {
+    onetime(document.getElementById('player-sound'), 'ended', function() {
  
         allEnemies = Resources.createEnemies(levels[that.level]);
         // random num between 0 and 4
         var xVal = Resources.getRandom(0,4);
         that.x = xVal * 101;
-        that.y = 410;
+        that.y = 413;
         that.playSound('../sound/jump.m4a');
         that.startTime = Date.now();
             
         if (that.lives > 0){
-            that.inState = "play"; 
+            that.inState = 'play';
+            goodie = new Goodie();
+            Resources.bgSound(that.music);
         } else {
-            that.inState = "over";
-            allEnemies[0].enemySound("pause"); // end music when game over         
+            that.inState = 'over';
+            Resources.bgSound(false);   // end music when game over      
         }
     });
     // create a one-time event
     // http://www.sitepoint.com/create-one-time-events-javascript/
     function onetime(node, type, callback) {
-
         // create event
         node.addEventListener(type, function(e) {
             // remove event
@@ -379,23 +427,30 @@ Player.prototype.restart = function (levelUp) {
             return callback(e);
         });
     }
-}
+};
 
+/****** GOODIES RELATED *****/
 
-var Heart = function (xStart, yStart) {
+var Goodie = function (xStart, yStart) {
+
+    var index = Resources.getRandom(0, goodies.length - 1); //get random 'goodie'
     this.sprite = 'images/Heart.png';
+    this.sprite = goodies[index].url;
+    this.life = goodies[index].life;
+    this.points = goodies[index].points;
     this.x = Resources.getRandom(0,4) * 101;
-    this.y = Resources.getRandom(0,2) * 84 + 66;
-}
+    this.y = Resources.getRandom(0,2) * 83 + 76;
+};
 
-Heart.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+Goodie.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x , this.y);
+};
+
 
 var player = new Player(),
-    allEnemies = Resources.createEnemies();
+    allEnemies = Resources.createEnemies(),
+    goodie = new Goodie();
 
-var heart = new Heart();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -405,16 +460,13 @@ document.addEventListener('keyup', function(e) {
         38: 'up',
         39: 'right',
         40: 'down',
-        82: 'r'
+        67: 'c', // 'c'hange character
+        72: 'h', // 'h'elp menu
+        77: 'm', // 'm'usic  on/off
+        82: 'r'  // 'r'estart new game
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// When document loaded
-if (window.addEventListener){ // W3C standard
-    window.addEventListener('load', Resources.showGame, false);
-} else if (window.attachEvent){ // Microsoft
-    window.attachEvent('onload', Resources.showGame); 
-}
-
+window.addEventListener('load', Resources.showGame, false);
